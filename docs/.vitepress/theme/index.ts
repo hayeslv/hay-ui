d347caaa375c94da93389faa0be04e812bdefaf0
lvhaizhou <417703682@qq.com>
2022-08-07 17:37:18 +0800

    引入tailwindcss

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,8 @@ import Theme from "vitepress/theme";
 import HelloWorld from "../../../src/components/HelloWorld.vue";
 import Test from "../../../src/components/Test";
 
+// import "vitepress-theme-demoblock/theme/styles/index.css";
+import "./demo-block.scss";
 import DemoBlock from "vitepress-theme-demoblock/components/DemoBlock.vue";
 import Demo from "vitepress-theme-demoblock/components/Demo.vue";
 
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 
+import "./index.scss";
+
 createApp(App).mount("#app");
