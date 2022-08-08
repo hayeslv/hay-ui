244737158522fe3163e5ef77a3e03e1adb2a38e4
lvhaizhou <417703682@qq.com>
2022-08-08 10:14:56 +0800

    按钮样式

--- src/button/src/button.tsx
+++ src/button/src/button.tsx
@@ -1,13 +1,19 @@
 // 真正的源码
 
-import { defineComponent } from "vue";
+import { defineComponent, toRefs } from "vue";
+import { buttonProps } from "./button-type";
 
 export default defineComponent({
   name: "HButton",
+  props: buttonProps,
   setup(props, { slots }) {
+    console.log(props);
+    const { type } = toRefs(props);
+    console.log(type);
+
     return () => {
       const defaultSlot = slots.default ?  slots.default() : "按钮";
-      return <button>{defaultSlot}</button>;
+      return <button class={`h-btn h-btn--${type.value}`}>{defaultSlot}</button>;
     };
   },
 });
